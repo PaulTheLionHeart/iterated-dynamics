7b052eb123de4b17e1cd7b4cd8764059939e9b73
Paul the LionHeart <pdeleeuw@deleeuw.com.au>
2024-10-01 21:49:26 +1000

    Minor fix on comment on bug in fractalb.cpp

--- libid/fractalb.cpp
+++ libid/fractalb.cpp
@@ -688,7 +688,7 @@ bool MandelbnSetup()
 bool MandelbfSetup()
 {
     // I suspect the following code should be somewhere in perform_worklist() to reset the setup routine to floating point
-    // when zooming out. Somehow the math type is restored and the bigflt memory restored, but the pinter to setup isn't.
+    // when zooming out. Somehow the math type is restored and the bigflt memory restored, but the pointer to setup isn't.
     if (g_bf_math == bf_math_type::NONE)    // kludge to prevent crash when math type = NONE and still call bigflt setup routine
         return MandelfpSetup();
     // this should be set up dynamically based on corners
