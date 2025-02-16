9e3cc58756132bb2bec669a7b1960b51647bca1b
Paul the LionHeart <pdeleeuw@deleeuw.com.au>
2025-02-16 20:52:03 +1100

    Turn off symmetry for perturbation.
    Lines 1047-1051 added to calcfrac.cpp.

--- libid/engine/calcfrac.cpp
+++ libid/engine/calcfrac.cpp
@@ -1044,13 +1044,11 @@ static void perform_work_list()
     {
         // per_image can override
         g_calc_type = g_cur_fractal_specific->calc_type;
-/*
         if (g_use_perturbation)
         {
             g_symmetry = SymmetryType::NONE;            // symmetry causes crashes in perturbation
         }
         else
-*/
         {
             g_symmetry = g_cur_fractal_specific->symmetry; //   calctype & symmetry
         }
